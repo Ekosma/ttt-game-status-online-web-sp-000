9b5a3b72d05f7c2b236d35e55d5a66b19f033dda
Evelyn Kosma <ezranaja@gmail.com>
2021-02-18 18:04:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,3 +37,11 @@ def won_board(board)
 def full?(board)
   board.all? { |index| index == "X" || index == "O" }
 end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
